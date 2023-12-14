8fce6047713fbc87956dead02bc4ba945bdfcad6
Yulian Aznal Hakiky <yulian.hakiky@gmedia.id>
2023-12-14 09:44:43 +0700

    change : configuration

--- front-end/src/helper/msal.js
+++ front-end/src/helper/msal.js
@@ -4,7 +4,7 @@ const msalConfig = {
   auth: {
     clientId: `${import.meta.env.VITE_CLIENT_ID}`,
     authority: `https://login.microsoftonline.com/${import.meta.env.VITE_TENANT_ID}`,
-    // redirectUri: "http://localhost:3000/auth/redirect",
+    redirectUri: "http://localhost:3000/auth/redirect",
   }
 };
 
